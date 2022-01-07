e7c77f7455ba4904e2bca97f7ec6acfc69d642da
Jeroen B <jeroenb04@live.com>
2022-01-07 16:59:12 +0100

    Setup Board & Initial Loop

---
+++ 3t.rb
@@ -0,0 +1,23 @@
+require 'pry-byebug'
+
+class Board
+  @@rows = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
+
+  def self.rows
+    @@rows
+  end
+end
+
+class PlayLoop
+
+end
+
+class Player
+
+end
+
+won = false
+
+until won
+
+end
